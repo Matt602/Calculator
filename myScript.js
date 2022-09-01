ba614e83e314c06541c802ad1fa84ff4490c0bc8
Matt602 <matthew.frierson062@gmail.com>
2022-09-01 11:15:03 -0500

    Created a function to handle the operations by calling the basic math operations

--- myScript.js
+++ myScript.js
@@ -18,3 +18,23 @@ function divide(a,b)
     return a / b;
 }
 
+function operate(op, a, b)
+{
+    if(op === 'add')
+    {
+        add(a,b);
+    }
+    else if(op === 'subtract')
+    {
+        subtract(a,b);
+    }
+    else if(op === 'multiply')
+    {
+        multiply(a,b);
+    }
+    else if(op === 'divide')
+    {
+        divide(a,b);
+    }
+
+}
