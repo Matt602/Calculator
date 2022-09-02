4b752e80281e3ac3a83748a45bcb04dd0f6db67f
Matt602 <matthew.frierson062@gmail.com>
2022-09-02 16:05:22 -0500

    Hooked up the math op buttons to be able to do math operations and to show the result of the expression inputted by the user.

--- myScript.js
+++ myScript.js
@@ -1,8 +1,10 @@
-let displayValue = '0';
+let displayValue = '';
+let queue = [];
 
 
 function add(a,b)
 {
+    console.log(typeof a);
     return a + b;
 }
 
@@ -18,37 +20,52 @@ function multiply(a,b)
 
 function divide(a,b)
 {
-    return a / b;
+    if(b === 0)
+    {
+        return 'Cannot divide by 0';
+    }
+    else
+    {
+        return a / b;
+    }
+    
 }
 
 function operate(op, a, b)
 {
     if(op === 'add')
     {
-        add(a,b);
+        
+        return add(a,b);
     }
     else if(op === 'subtract')
     {
-        subtract(a,b);
+        return subtract(a,b);
     }
     else if(op === 'multiply')
     {
-        multiply(a,b);
+        return multiply(a,b);
     }
     else if(op === 'divide')
     {
-        divide(a,b);
+        return divide(a,b);
     }
 
 }
 
-function updateDisplay(text)
+function updateDisplay(text, resetDisplay)
 {
-    if(displayValue === '0')
+    
+    if(resetDisplay)
+    {
+        displayValue = text + '';
+        
+    }
+    else if(displayValue === '' && !resetDisplay)
     {
         displayValue = text + '';
     }
-    else
+    else 
     {
         displayValue = displayValue + text;
     }
@@ -56,17 +73,33 @@ function updateDisplay(text)
     dis.textContent = displayValue;
 }
 
-function clearDisplay()
+function clear()
 {
-    displayValue = '0';
-    updateDisplay(' ');
+    displayValue = '';
+    updateDisplay(' ', true);
+    queue = [];
+}
+
+function checkQueue()
+{
+    console.log(queue);
+    if(queue.length >= 3)
+    {
+        let result = operate(queue[1], parseInt(queue[0]), parseInt(queue[2]));
+        queue = queue.slice(2);
+        
+        queue[0] = result + '';
+        console.log(queue);
+        updateDisplay(result + '', true);
+        displayValue = '';
+    }
 }
 
 const buttons = document.querySelectorAll('.digit');
 
 buttons.forEach( button => {
     button.addEventListener('click', () => {
-        updateDisplay(button.textContent);
+        updateDisplay(button.textContent, false);
 
     });
 
@@ -74,5 +107,60 @@ buttons.forEach( button => {
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
-    clearDisplay();
+    clear();
+});
+
+const addButton = document.querySelector('.add');
+addButton.addEventListener('click', () => {
+    if(queue.length === 0 || queue.length === 2)
+    {
+        queue.push(displayValue);
+        displayValue = '';
+    } 
+    displayValue = '';
+    queue.push('add');
+    checkQueue();
+});
+
+const subtractButton = document.querySelector('.subtract');
+subtractButton.addEventListener('click', () => {
+    if(queue.length === 0 || queue.length === 2)
+    {
+        queue.push(displayValue);
+        displayValue = '';
+    } 
+    displayValue = '';
+    queue.push('subtract');
+    checkQueue();
+});
+
+const multiplyButton = document.querySelector('.multiply');
+multiplyButton.addEventListener('click', () => {
+    if(queue.length === 0 || queue.length === 2)
+    {
+        queue.push(displayValue);
+        displayValue = '';
+    } 
+    displayValue = '';
+    queue.push('multiply');
+    checkQueue();
+});
+
+const divideButton = document.querySelector('.divide');
+divideButton.addEventListener('click', () => {
+    if(queue.length === 0 || queue.length === 2)
+    {
+        queue.push(displayValue);
+        displayValue = '';
+    } 
+    displayValue = '';
+    queue.push('divide');
+    checkQueue();
+});
+
+const equalButton = document.querySelector('.equal');
+equalButton.addEventListener('click', () => {
+    queue.push(displayValue);
+    checkQueue();
+
 });
